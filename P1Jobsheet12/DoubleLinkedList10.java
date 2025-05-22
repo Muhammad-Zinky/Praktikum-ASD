37e3ea3f704d0fa2c120a5d959dbebc2d187af8c
mohammad zinky <mohammadzinky@gmail.com>
2025-05-22 11:30:33 +0700

    Jobsheet 12

---
+++ P1Jobsheet12/DoubleLinkedList10.java
@@ -0,0 +1,73 @@
+package P1Jobsheet12;
+
+public class DoubleLinkedList10 {
+    Node10 head;
+    Node10 tail;
+
+    public DoubleLinkedList10() {
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(Mahasiswa10 data) {
+        Node10 newNode = new Node10(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            head.next = newNode;
+            newNode.prev = head;
+            head = newNode;
+        }
+    }
+
+    public void addLast(Mahasiswa10 data) {
+        Node10 newNode = new Node10(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void insertAfter(String keyNim, Mahasiswa10 data) {
+        Node10 current = head;
+
+        while (current != null && !current.data.nim.equals(keyNim)) {
+            current = current.next;
+        }
+
+        if (current == null) {
+            System.out.println("Node dengan NIM " + keyNim + " tidak ditemukan.");
+            return;
+        }
+
+        Node10 newNode = new Node10(data);
+
+        if(current == tail) {
+            current.next = newNode;
+            newNode.prev = current;
+            tail = newNode;
+        } else {
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+
+        System.out.println("Node berhasil disisipkan setelah NIM" + keyNim);
+    }
+
+    public void print() {
+        Node10 current = head;
+        while (current != null) {
+            current.data.tampil();
+            current = current.next;
+        }
+    }
+}
