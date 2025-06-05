2f79eeb02203d98ebfc4c1e0e0b89c0e217229d6
mohammad zinky <mohammadzinky@gmail.com>
2025-06-05 11:08:20 +0700

    Pertemuan14

---
+++ Pertemuan14/Node10.java
@@ -0,0 +1,15 @@
+package Pertemuan14;
+
+public class Node10 {
+    Mahasiswa10 mahasiswa10;
+    Node10 left, right;
+
+    public Node10(){
+    }
+    
+    public Node10(Mahasiswa10 mahasiswa10) {
+        this.mahasiswa10 = mahasiswa10;
+        left  = right = null;
+    }
+}
+
